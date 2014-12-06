df5c30a6d3db6e5747d8bd61b83723d814543c34
Дмитрий Пацура <talk@dmtry.me>
2014-12-06 14:19:17 +0900

    Added install script for Zephir

---
+++ vagrant/scripts/zephir.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Installing Zephir..."
+
+sudo apt-get install -y git gcc make re2c php5 php5-dev libpcre3-dev
+git clone https://github.com/phalcon/zephir
+cd zephir
+./install-json
+./install -c

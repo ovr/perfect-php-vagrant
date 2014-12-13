a84c3dbd18b49485fd60e7f6073874bf9ced57aa
Дмитрий Пацура <talk@dmtry.me>
2014-12-13 14:32:43 +0900

    Remove running for install scripts

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -5,10 +5,3 @@ sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 
 sudo apt-get install -y git
-
-SCRIPTS_PATH=/vagrant/vagrant/scripts
-
-${SCRIPTS_PATH}/nginx.sh
-${SCRIPTS_PATH}/php.sh
-${SCRIPTS_PATH}/zephir.sh
-${SCRIPTS_PATH}/phalcon.sh

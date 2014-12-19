3d636f51ff189b18abca104f0f9719b27bcb868c
Дмитрий Пацура <talk@dmtry.me>
2014-12-19 13:28:30 +0900

    Install composer

--- vagrant/scripts/php.sh
+++ vagrant/scripts/php.sh
@@ -2,7 +2,7 @@
 
 echo "Installing PHP..."
 
-sudo apt-get -y install php5-cli php5-dev php5-fpm
+sudo apt-get -y install php5-cli php5-dev php5-fpm php5-curl
 
 sudo curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer

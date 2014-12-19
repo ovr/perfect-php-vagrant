7fdb03a9b53944fec276eb3e4cdb1b4a09b26fcc
Дмитрий Пацура <talk@dmtry.me>
2014-12-19 18:55:24 +0900

    Added Lynx version for localhost

--- localhost/index.php
+++ localhost/index.php
@@ -4,5 +4,10 @@ echo "PHP Version: " . phpversion() . " </br>";
 
 if (extension_loaded('Phalcon')) {
     $ext = new ReflectionExtension('phalcon');
-    echo "Phalcon Version: " . $ext->getVersion();
+    echo "Phalcon Version: " . $ext->getVersion() . "</br>";
+}
+
+if (extension_loaded('Lynx')) {
+    $ext = new ReflectionExtension('lynx');
+    echo "Lynx Version: " . $ext->getVersion() . "</br>";
 }

f99139b9c50e1a3fda472916aa38988cf1acd182
Дмитрий Пацура <talk@dmtry.me>
2014-12-19 18:06:48 +0900

    Install nginx.sh

--- vagrant/scripts/nginx.sh
+++ vagrant/scripts/nginx.sh
@@ -3,3 +3,9 @@
 echo "Installing Nginx..."
 
 sudo apt-get -y install nginx-full
+
+sudo cp /vagrant/vagrant/etc/nginx/nginx.conf /etc/nginx/nginx.conf
+sudo rm -rf /etc/nginx/sites-enabled/*
+sudo cp /vagrant/vagrant/etc/nginx/sites-enabled/* /etc/nginx/sites-enabled/
+
+sudo service nginx restart

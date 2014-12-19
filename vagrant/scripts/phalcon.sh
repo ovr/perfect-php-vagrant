2d42eeca1cd5aa16b01342c689f2f1ecfc4177de
Дмитрий Пацура <talk@dmtry.me>
2014-12-19 13:28:05 +0900

    Speedup phalcon installation via tar archive

--- vagrant/scripts/phalcon.sh
+++ vagrant/scripts/phalcon.sh
@@ -3,9 +3,15 @@
 echo "Installing Phalcon 2..."
 
 cd /tmp
-git clone -b 2.0.0 https://github.com/phalcon/cphalcon.git
-cd cphalcon
+wget -o /dev/null https://github.com/phalcon/cphalcon/archive/2.0.0.zip
+unzip 2.0.0.zip -d cphalcon > /dev/null
+rm 2.0.0.zip
+cd cphalcon/cphalcon-2.0.0
 zephir install
+
+#git clone -b 2.0.0 https://github.com/phalcon/cphalcon.git
+#cd cphalcon
+#zephir install
 
 echo extension=phalcon.so > /etc/php5/cli/conf.d/30-phalcon.ini
 echo extension=phalcon.so > /etc/php5/fpm/conf.d/30-phalcon.ini

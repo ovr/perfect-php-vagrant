82ec7fe9be6fe521e0ac2e5100b442221b288222
Дмитрий Пацура <talk@dmtry.me>
2014-12-19 13:27:42 +0900

    Remove unrar, use google public dns

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+echo "nameserver 8.8.8.8" >> /etc/resolvconf/resolv.conf.d/head
+echo "nameserver 8.8.4.4" >> /etc/resolvconf/resolv.conf.d/head
+sudo service resolvconf restart
+
 sudo apt-get -y update
+
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 
-sudo apt-get install -y git unzip unrar
+sudo apt-get install -y git unzip

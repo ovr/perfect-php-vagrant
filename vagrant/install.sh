707a39f8009cd127b72e32c20729688ecfe182d6
Дмитрий Пацура <talk@dmtry.me>
2014-12-20 11:22:54 +0900

    Install memcached and php5-memcache

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -9,4 +9,4 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 
-sudo apt-get install -y git unzip htop
+sudo apt-get install -y git unzip htop memcached
--- vagrant/scripts/php.sh
+++ vagrant/scripts/php.sh
@@ -2,7 +2,7 @@
 
 echo "Installing PHP..."
 
-sudo apt-get -y install php5-cli php5-dev php5-fpm php5-curl
+sudo apt-get -y install php5-cli php5-dev php5-fpm php5-curl php5-memcache
 
 sudo curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer

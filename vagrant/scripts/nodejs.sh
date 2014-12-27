79dcb3cde39b607e97ba8d45500d8112ef65c6c0
Дмитрий Пацура <talk@dmtry.me>
2014-12-27 11:29:09 +0900

    NPM update NPM

--- vagrant/scripts/nodejs.sh
+++ vagrant/scripts/nodejs.sh
@@ -4,6 +4,7 @@ echo "Installing NodeJS..."
 
 sudo apt-get install -y nodejs npm
 sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo npm -g update npm
 
 echo "Installing Gulp and Bower for global env..."
 

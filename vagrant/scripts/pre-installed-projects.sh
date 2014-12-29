20f8f61674b8b1b5317824596e160e5dd5cfc370
Дмитрий Пацура <talk@dmtry.me>
2014-12-29 10:10:53 +0900

    Fix #2 issue - wrong angular skeleton installation

--- vagrant/scripts/pre-installed-projects.sh
+++ vagrant/scripts/pre-installed-projects.sh
@@ -5,7 +5,7 @@ if [ ! -d "/var/www/projects/ovr" ]; then
 fi
 
 if [ ! -d "/var/www/projects/ovr/angular-skeleton" ]; then
-    git clone https://github.com/ovr/angular-skeleton.git ./var/www/projects/ovr/angular-skeleton
+    git clone https://github.com/ovr/angular-skeleton.git /var/www/projects/ovr/angular-skeleton
 fi
 
 if [ ! -d "/var/www/projects/ovr/phalcon-module-skeleton" ]; then

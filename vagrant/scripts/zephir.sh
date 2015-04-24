89232f85d426e78487623a70c100bf536e4507f5
Dmitry Patsura <talk@dmtry.me>
2015-04-24 19:09:11 +0900

    Remove unneeded php5 relation
    - via php5 install apache..

--- vagrant/scripts/zephir.sh
+++ vagrant/scripts/zephir.sh
@@ -2,7 +2,7 @@
 
 echo "Installing Zephir..."
 
-sudo apt-get install -y git gcc make re2c php5 php5-dev libpcre3-dev
+sudo apt-get install -y git gcc make re2c php5-dev libpcre3-dev
 
 cd ~/build
 

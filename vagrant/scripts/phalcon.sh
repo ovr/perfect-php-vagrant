1b5555f4829a3a87f69585be01bb6058d1645d6e
Dmitry Patsura <zaets28rus@gmail.com>
2015-06-05 22:16:31 +0800

    Fetch latest master when install Phalcon

--- vagrant/scripts/phalcon.sh
+++ vagrant/scripts/phalcon.sh
@@ -3,10 +3,10 @@
 echo "Installing Phalcon 2..."
 
 cd ~/build
-wget -o /dev/null https://github.com/phalcon/cphalcon/archive/2.0.0.zip
-unzip 2.0.0.zip -d cphalcon > /dev/null
-rm 2.0.0.zip
-cd cphalcon/cphalcon-2.0.0
+wget -o /dev/null -O phalcon.zip https://github.com/phalcon/cphalcon/archive/master.zip
+unzip phalcon.zip -d cphalcon > /dev/null
+rm phalcon.zip
+cd cphalcon/cphalcon-master
 zephir install
 
 #git clone -b 2.0.0 https://github.com/phalcon/cphalcon.git
